d7b218306b005beed0a6537231eeb3568070eab3
Zubrzycki Kévin <kvin.zubrzycki@gmail.com>
2021-03-15 15:07:12 +0100

    Liaison formulaire avec du php vers bdd -> Ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,11 +65,3 @@ function resetImageMontauban() {
 }
 
 // Formulaire
-document
-  .querySelector("#contact-form")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
-    e.target.elements.name.value = "";
-    e.target.elements.email.value = "";
-    e.target.elements.message.value = "";
-  });
